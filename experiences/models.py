3220fe1058fd67d81588145c353ccf504777764e
hanjuren <juren528@gmail.com>
2022-10-23 18:10:47 +0900

    Experience, Perk 모델 생성, 마이그레이션

--- experiences/models.py
+++ experiences/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+from common.models import CommonModel
+from django.conf import settings
 
-# Create your models here.
+
+class Experience(CommonModel):
+
+    """Experience Model Definition"""
+
+    name = models.CharField(
+        max_length=250,
+    )
+    description = models.TextField()
+    country = models.CharField(
+        max_length=50,
+        default="한국",
+    )
+    price = models.PositiveIntegerField()
+    city = models.CharField(
+        max_length=80,
+        default="서울",
+    )
+    host = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    address = models.CharField(
+        max_length=250,
+    )
+    starts_at = models.TimeField()
+    ends_at = models.TimeField()
+    perks = models.ManyToManyField(
+        "experiences.Perk",
+    )
+
+    class Meta:
+        db_table = "experiences"
+
+
+class Perk(CommonModel):
+
+    """What is included on an Experiences"""
+
+    name = models.CharField(
+        max_length=100,
+    )
+    details = models.CharField(
+        max_length=250,
+    )
+    explanation = models.TextField()
+
+    class Meta:
+        db_table = "perks"
