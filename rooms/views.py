8469f48e1b55de07fe18150b5f3793bef885c485
hanjuren <juren528@gmail.com>
2022-11-03 20:48:15 +0900

    #9.3 ~ #9.5 template 사용 해보기

--- rooms/views.py
+++ rooms/views.py
@@ -1,14 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-
-def say_hello(request):
-    return HttpResponse("hello")
+from .models import Room
 
 
 def see_all_rooms(request):
-    return HttpResponse("See all rooms")
+    queryset = Room.objects.all()
+    return render(
+        request,
+        "all_rooms.html",
+        {
+            'rooms': queryset,
+            'title': "Hello! this title comes from django!",
+        }
+    )
 
 
-def see_one_room(request, room_id):
-    return HttpResponse(f"See room with id {room_id}")
+def see_one_room(request, room_pk):
+    try:
+        room = Room.objects.get(pk=room_pk)
+        return render(request, "room_detail.html", {'room': room})
+    except Room.DoesNotExist:
+        return render(request, "room_detail.html", {'not_found': True})
+
