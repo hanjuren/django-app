70bef5b3cc532aeabc6cc430f16bec6083d662fd
hanjuren <juren528@gmail.com>
2022-11-09 21:29:17 +0900

    #11.0 ~ #11.1 Amenities and AmenityDetail API

--- rooms/urls.py
+++ rooms/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-
+    path("amenities/", views.Amenities.as_view()),
+    path("amenities/<int:pk>/", views.AmenityDetail.as_view()),
 ]
--- rooms/views.py
+++ rooms/views.py
@@ -1,2 +1,53 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.status import HTTP_204_NO_CONTENT, HTTP_422_UNPROCESSABLE_ENTITY
+from rest_framework.exceptions import NotFound
+from .models import Amenity
+from .serializers import AmenitySerializer
 
+
+class Amenities(APIView):
+
+    def get(self, request):
+        amenities = Amenity.objects.all()
+        serializer = AmenitySerializer(amenities, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = AmenitySerializer(data=request.data)
+        if serializer.is_valid():
+            amenity = serializer.save()
+            return Response(
+                AmenitySerializer(amenity).data
+            )
+        else:
+            return Response(serializer.errors, status=HTTP_422_UNPROCESSABLE_ENTITY)
+
+
+class AmenityDetail(APIView):
+
+    def get_amenity(self, pk):
+        try:
+            return Amenity.objects.get(pk=pk)
+        except Amenity.DoesNotExist:
+            raise NotFound
+
+    def get(self, request, pk):
+        amenity = self.get_amenity(pk)
+        serializer = AmenitySerializer(amenity)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        amenity = self.get_amenity(pk)
+        serializer = AmenitySerializer(amenity, data=request.data, partial=True)
+        if serializer.is_valid():
+            amenity = serializer.save()
+            serializer = AmenitySerializer(amenity)
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=HTTP_422_UNPROCESSABLE_ENTITY)
+
+    def delete(self, request, pk):
+        amenity = self.get_amenity(pk)
+        amenity.delete()
+        return Response(status=HTTP_204_NO_CONTENT)
