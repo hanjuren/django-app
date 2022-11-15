094ce14949d2c45f69063d3e86d021720ef35a43
hanjuren <juren528@gmail.com>
2022-11-15 14:53:01 +0900

    #11.2 Perks, PerkDetail API

--- experiences/views.py
+++ experiences/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status, exceptions
+from .models import Perk
+from .serializers import PerkSerializer
 
-# Create your views here.
+
+class Perks(APIView):
+
+    def get(self, request):
+        perks = Perk.objects.all()
+        serializer = PerkSerializer(perks, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = PerkSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+
+
+class PerkDetail(APIView):
+
+    def get_object(self, pk):
+        try:
+            return Perk.objects.get(pk=pk)
+        except Perk.DoesNotExist:
+            raise exceptions.NotFound
+
+    def get(self, request, pk):
+        perk = self.get_object(pk)
+        serializer = PerkSerializer(perk)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        perk = self.get_object(pk)
+        serializer = PerkSerializer(perk, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+
+    def delete(self, request, pk):
+        perk = self.get_object(pk)
+        perk.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
