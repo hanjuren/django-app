eb6c2e78bff63f4ffad9d6cddcf467beb99bd8cb
hanjuren <juren528@gmail.com>
2022-12-15 19:32:29 +0900

    GET, PUT, DELETE /experiences/1/bookings/1

--- experiences/urls.py
+++ experiences/urls.py
@@ -4,8 +4,9 @@ from . import views
 urlpatterns = [
     path("", views.ExperienceList.as_view()),
     path("<int:pk>/", views.ExperienceDetail.as_view()),
-    path("<int:pk>/bookings/", views.ExperienceBookings.as_view()),
     path("<int:pk>/perks/", views.ExperiencePerks.as_view()),
+    path("<int:pk>/bookings/", views.ExperienceBookings.as_view()),
+    path("<int:pk>/bookings/<int:booking_pk>/", views.ExperienceBookingDetail.as_view()),
     path("perks/", views.Perks.as_view()),
     path("perks/<int:pk>/", views.PerkDetail.as_view()),
 ]
