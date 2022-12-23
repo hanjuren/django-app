2b71565e80c7c9caa2ed60b0060dfa010124f28d
hanjuren <juren528@gmail.com>
2022-12-23 13:30:44 +0900

    14.5 Django Types #3

--- config/schema.py
+++ config/schema.py
@@ -1,46 +1,18 @@
 import strawberry
-import typing
+from rooms import schema as room_schema
 
 
 @strawberry.type
-class Movie:
-    pk: int
-    title: str
-    year: int
-    rating: int
-
-
-movies_db = [
-    Movie(pk=1, title='Gotfather', year=2022, rating=4),
-]
-
-
-@strawberry.type
-class Query:
-    movies: typing.List[Movie] = strawberry.field(resolver=movies)
-    movie: Movie = strawberry.field(resolver=movie)
+class Query(room_schema.Query):
+    pass
 
 
 @strawberry.type
 class Mutation:
-    add_movie: Movie = strawberry.mutation(resolver=add_movie)
-
-
-def movies():
-    return movies_db
-
-
-def movie(movie_pk: int):
-    return movies_db[movie_pk - 1]
-
-
-def add_movie(title: str, year: int, rating: int) -> Movie:
-    new_movie = Movie(pk=len(movies_db) + 1, title=title, year=year, rating=rating)
-    movies_db.append(new_movie)
-    return new_movie
+    pass
 
 
 schema = strawberry.Schema(
     query=Query,
-    mutation=Mutation,
+    # mutation=Mutation,
 )
