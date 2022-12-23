c62f280f7fe2b29560bcfbc4c5bcb894ef5d126a
hanjuren <juren528@gmail.com>
2022-12-23 13:03:58 +0900

    14.3 Mutation #3

--- config/schema.py
+++ config/schema.py
@@ -1,11 +1,41 @@
 import strawberry
+import typing
+
+
+@strawberry.type
+class Movie:
+    pk: int
+    title: str
+    year: int
+    rating: int
+
+
+movies_db = [
+    Movie(pk=1, title='Gotfather', year=2022, rating=4),
+]
 
 
 @strawberry.type
 class Query:
     @strawberry.field
-    def ping(self) -> str:
-        return "pong"
+    def movies(self) -> typing.List[Movie]:
+        return movies_db
+
+    @strawberry.field
+    def movie(self, movie_pk: int) -> Movie:
+        return movies_db[movie_pk - 1]
 
 
-schema = strawberry.Schema(query=Query)
+@strawberry.type
+class Mutation:
+    @strawberry.mutation
+    def add_movie(self, title: str, year: int, rating: int) -> Movie:
+        new_movie = Movie(pk=len(movies_db) + 1, title=title, year=year, rating=rating)
+        movies_db.append(new_movie)
+        return new_movie
+
+
+schema = strawberry.Schema(
+    query=Query,
+    mutation=Mutation,
+)
