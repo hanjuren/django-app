834b9a7705a6e04cfe808950c9f8de586e0486a9
hanjuren <juren528@gmail.com>
2023-03-12 20:04:18 +0900

    model test #6

--- tests/models/room_test.py
+++ tests/models/room_test.py
@@ -1,8 +1,33 @@
 import pytest
+from tests.factories import *
 from users.models import User
 
 pytestmark = pytest.mark.django_db
 
 
-def test_room1():
-    pass
+class TestRoom:
+    @pytest.fixture(autouse=True)
+    def setup(self):
+        self.user = UserFactory.create()
+        self.room = RoomFactory.create(
+            name="테스트 룸",
+            owner=self.user
+        )
+
+    def test_total_amenities(self):
+        assert self.room.total_amenities() == 0
+
+        amenity = AmenityFactory.create()
+        self.room.amenities.add(amenity)
+
+        assert self.room.total_amenities() == 1
+
+    def test_rating(self):
+        assert self.room.rating() == 0
+
+        review = ReviewFactory.create(
+            user=self.user,
+            room=self.room,
+        )
+
+        assert self.room.rating() == 4
