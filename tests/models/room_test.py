959675951598f38a50440b04f7964dee27136544
hanjuren <juren528@gmail.com>
2023-03-15 12:39:44 +0900

    fixtures and factory use in class #6

--- tests/models/room_test.py
+++ tests/models/room_test.py
@@ -1,40 +1,39 @@
 import pytest
-from tests.factories import *
 
 pytestmark = pytest.mark.django_db
 
 
 class TestRoom:
     @pytest.fixture(autouse=True)
-    def before(self):
-        self.user = UserFactory.create()
-        self.room = RoomFactory.create(
+    def set_up(self, user_factory, room_factory):
+        self.user = user_factory.create()
+        self.room = room_factory.create(
             name="테스트 룸",
             owner=self.user
         )
 
-    def test_total_amenities(self):
+    def test_total_amenities(self, amenity_factory):
         assert self.room.total_amenities() == 0
 
-        amenity = AmenityFactory.create()
+        amenity = amenity_factory.create()
         self.room.amenities.add(amenity)
 
         assert self.room.total_amenities() == 1
 
-    def test_rating(self):
+    def test_rating(self, review_factory):
         assert self.room.rating() == 0
 
-        review = ReviewFactory.create(
+        review = review_factory.create(
             user=self.user,
             room=self.room,
         )
 
         assert self.room.rating() == 4
 
-    def test_add_amenities(self):
+    def test_add_amenities(self, amenity_factory):
         amenity_ids = []
         for name in ["test amenity 1", "test amenity 2"]:
-            amenity = AmenityFactory.create(name=name)
+            amenity = amenity_factory.create(name=name)
             amenity_ids.append(amenity.pk)
         self.room.add_amenities(amenity_ids)
 
