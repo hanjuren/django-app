cde384ebb2b977e7b7da4ee8200c0cbb1e20f225
hanjuren <juren528@gmail.com>
2023-03-24 19:55:11 +0900

    sing_in api test #6

--- tests/api/auth_api_test.py
+++ tests/api/auth_api_test.py
@@ -1,5 +1,6 @@
 import pytest
 import json
+import re
 from users.models import User
 
 pytestmark = pytest.mark.django_db
@@ -50,5 +51,49 @@ class TestSignUp:
         assert res.status_code == 201
         assert User.objects.count() == 1
         assert data['success'] is True
-        assert data['token'] is not None
-        assert type(data['token']) == str
+
+
+class TestSignIn:
+    """
+    로그인 테스트
+    """
+    @pytest.fixture(autouse=True)
+    def set_up(self, client):
+        self.url_prefix = "/api/v1/auth/sign_in/"
+        self.client = client
+
+    def test_email_is_required(self):
+        params = {"email": "juren52@naver.com"}
+        res = self.client.post(self.url_prefix, params)
+        data = json.loads(res.content)
+
+        assert res.status_code == 400
+        assert re.match("잘못된 요청입니다.", data['error']['message'])
+
+    def test_password_is_required(self):
+        params = {"password": "q1w2e3r4"}
+        res = self.client.post(self.url_prefix, params)
+        data = json.loads(res.content)
+
+        assert res.status_code == 400
+        assert re.match("잘못된 요청입니다.", data['error']['message'])
+
+    def test_not_found_user(self):
+        params = {"email": "juren52@naver.com", "password": "q1w2e3r4"}
+        res = self.client.post(self.url_prefix, params)
+        data = json.loads(res.content)
+
+        assert res.status_code == 404
+        assert re.match("Not Found", data['error']['message'])
+
+    def test_sign_in(self):
+        sign_up_params = {"email": "juren52@naver.com", "password": "q1w2e3r4"}
+        self.client.post("/api/v1/auth/sign_up/", sign_up_params)
+
+        sign_in_params = sign_up_params.copy()
+        res = self.client.post(self.url_prefix, sign_in_params)
+        data = json.loads(res.content)
+
+        assert res.status_code == 201
+        assert data['success'] is True
+        assert data['access_token'] is not None
