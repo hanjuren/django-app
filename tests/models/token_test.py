965885f3bc028885174f5ea9a4f25102158dd1cf
hanjuren <juren528@gmail.com>
2023-04-06 20:36:15 +0900

    #11 token 모델 테스트

--- tests/models/token_test.py
+++ tests/models/token_test.py
@@ -1,10 +1,9 @@
 import re
-
 import jwt
 import pytest
 from django.conf import settings
 from rest_framework.exceptions import AuthenticationFailed
-from authorization import tokens
+from authorization.models import Token
 from datetime import datetime, timedelta, timezone
 
 pytestmark = pytest.mark.django_db
@@ -14,15 +13,15 @@ def test_gen_jwt_token_invalid_token_type():
     payload = {}
     token_type = "invalid"
     with pytest.raises(ValueError) as err:
-        tokens.gen_jwt_token(payload, token_type)
+        Token.gen_jwt_token(payload, token_type)
     assert str(err.value) == "Invalid Token Type."
 
 
 def test_gen_jwt_token_access_token():
     payload = {"id": 1, "email": "juren52@naver.com"}
     token_type = "access"
-    access_token = tokens.gen_jwt_token(payload, token_type)
-    decoded_token = tokens.decoded_jwt_token(access_token)
+    access_token = Token.gen_jwt_token(payload, token_type)
+    decoded_token = Token.decoded_jwt_token(access_token)
 
     assert decoded_token['id'] == 1
     assert decoded_token['email'] == "juren52@naver.com"
@@ -40,8 +39,8 @@ def test_gen_jwt_token_access_token():
 def test_gen_jwt_token_refresh_token():
     payload = {"id": 1, "email": "juren52@naver.com"}
     token_type = "refresh"
-    refresh_token = tokens.gen_jwt_token(payload, token_type)
-    decoded_token = tokens.decoded_jwt_token(refresh_token)
+    refresh_token = Token.gen_jwt_token(payload, token_type)
+    decoded_token = Token.decoded_jwt_token(refresh_token)
 
     assert decoded_token['id'] == 1
     assert decoded_token['email'] == "juren52@naver.com"
@@ -65,8 +64,8 @@ def test_decoded_jwt_token_with_error():
     token = jwt.encode(payload, settings.SECRET_KEY, algorithm="HS256")
 
     with pytest.raises(AuthenticationFailed, match=re.escape("Signature has expired")):
-        tokens.decoded_jwt_token(token)
+        Token.decoded_jwt_token(token)
 
     token = jwt.encode({'some': 'payload'}, settings.SECRET_KEY, algorithm='HS256')
     with pytest.raises(AuthenticationFailed, match=re.escape("Signature verification failed")):
-        tokens.decoded_jwt_token(token[:-3] + 'xyz')
+        Token.decoded_jwt_token(token[:-3] + 'xyz')
