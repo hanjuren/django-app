4cc07438d23e7f72a6a3a28002548bd7adfca147
hanjuren <juren528@gmail.com>
2023-06-04 14:58:45 +0900

    admin custom filter #17

--- reviews/admin.py
+++ reviews/admin.py
@@ -2,6 +2,49 @@ from django.contrib import admin
 from .models import Review
 
 
+class WordFilter(admin.SimpleListFilter):
+    title = "Filter by words."
+    parameter_name = "word"
+
+    def lookups(self, request, model_admin) -> list[tuple]:
+        return [
+            ("good", "Good"),
+            ("great", "Great"),
+            ("awesome", "Awesome"),
+            ("bad", "Bad"),
+        ]
+
+    def queryset(self, request, queryset) -> list[Review]:
+        word = request.GET.get('word')
+
+        if word:
+            return queryset.filter(payload__contains=word)
+        else:
+            return queryset
+
+
+class SatisFactionFilter(admin.SimpleListFilter):
+    title = "Filter by satisfaction."
+    parameter_name = "satisfaction"
+
+    def lookups(self, request, model_admin) -> list[tuple]:
+        return [
+            ("bad", "Bad Reviews"),
+            ("good", "Good Reviews")
+        ]
+
+    def queryset(self, request, queryset) -> list[Review]:
+        q = queryset
+        satisfaction = request.GET.get('satisfaction')
+
+        if satisfaction == "good":
+            q = q.filter(rating__gte=3)
+        elif satisfaction == "bad":
+            q = q.filter(rating__lt=3)
+
+        return q
+
+
 class ReviewAdmin(admin.ModelAdmin):
     list_display = (
         "__str__",
@@ -9,7 +52,10 @@ class ReviewAdmin(admin.ModelAdmin):
         "created_at",
         "updated_at",
     )
-    list_filter = ("rating",)
+    list_filter = (
+        WordFilter,
+        SatisFactionFilter,
+    )
 
 
 admin.site.register(Review, ReviewAdmin)
