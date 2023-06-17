a9df22a989c10b14a38ef38788bcbf3826959e7e
hanjuren <juren528@gmail.com>
2023-06-17 14:00:37 +0900

    rest framework ModelSerializer #21

--- categories/serializers/response_serializer.py
+++ categories/serializers/response_serializer.py
@@ -1,9 +1,8 @@
 from rest_framework import serializers
+from categories.models import Category
 
 
-class CategoryResponseSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    name = serializers.CharField(required=True)
-    kind = serializers.CharField()
-    created_at = serializers.DateTimeField()
-    updated_at = serializers.DateTimeField()
+class CategoryResponseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = "__all__"
