6fb7d5b4b780d1f88cbe51afdf43f0a6293118a9
hanjuren <juren528@gmail.com>
2023-06-17 13:05:09 +0900

    rest framework update, delete #21

--- categories/serializers/validation_serializers.py
+++ categories/serializers/validation_serializers.py
@@ -8,3 +8,12 @@ class CategoryCreationSerializer(serializers.Serializer):
 
     def create(self, validated_data):
         return Category.objects.create(**validated_data)
+
+
+class CategoryUpdateSerializer(CategoryCreationSerializer):
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get("name", instance.name)
+        instance.kind = validated_data.get("kind", instance.kind)
+        instance.save()
+
+        return instance
--- categories/views.py
+++ categories/views.py
@@ -1,7 +1,9 @@
 from rest_framework.decorators import api_view
+from rest_framework.exceptions import NotFound
+from rest_framework.status import HTTP_204_NO_CONTENT, HTTP_201_CREATED, HTTP_422_UNPROCESSABLE_ENTITY
 from rest_framework.response import Response
 from .models import Category
-from .serializers import CategoryResponseSerializer, CategoryCreationSerializer
+from .serializers import CategoryResponseSerializer, CategoryCreationSerializer, CategoryUpdateSerializer
 
 
 @api_view(["GET", "POST"])
@@ -19,14 +21,37 @@ def all_categories(request):
         serializer = CategoryCreationSerializer(data=request.data)
 
         if not serializer.is_valid():
-            return Response(serializer.errors)
+            return Response(serializer.errors, status=HTTP_422_UNPROCESSABLE_ENTITY)
         else:
             category = serializer.save()
-            return Response(CategoryResponseSerializer(category).data)
+            return Response(
+                CategoryResponseSerializer(category).data,
+                status=HTTP_201_CREATED,
+            )
 
 
-@api_view()
+@api_view(["GET", "PUT", "DELETE"])
 def get_category(request, id_):
-    category = Category.objects.get(id=id_)
+    try:
+        category = Category.objects.get(id=id_)
+    except Category.DoesNotExist:
+        raise NotFound
 
-    return Response(CategoryResponseSerializer(category).data)
+    if request.method == "GET":
+        return Response(CategoryResponseSerializer(category).data)
+    elif request.method == "PUT":
+        serializer = CategoryUpdateSerializer(
+            category,
+            data=request.data,
+            partial=True,
+        )
+
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=HTTP_422_UNPROCESSABLE_ENTITY)
+        else:
+            serializer.save()
+            return Response(CategoryResponseSerializer(category).data)
+    elif request.method == "DELETE":
+        category.delete()
+        return Response(status=HTTP_204_NO_CONTENT)
+
