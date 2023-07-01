b634ee67e23309b1fb59ad29959e6f34123e7b04
hanjuren <juren528@gmail.com>
2023-07-01 16:28:19 +0900

    APPEND_SLASH = False #23

--- rooms/urls.py
+++ rooms/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     # path("", views.see_all_rooms),
     # path("<int:id_>", views.see_one_room),
     # path("<str:name>", views.see_one_order),
-    path("amenities", views.Amenities.as_view()),
-    path("amenities/<int:id_>", views.AmenityDetail.as_view()),
+    path("/amenities", views.Amenities.as_view()),
+    path("/amenities/<int:id_>", views.AmenityDetail.as_view()),
 ]
