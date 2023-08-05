7e99f30bda07ad3b42b5b2cf431110007d78eef4
hanjuren <juren528@gmail.com>
2023-08-05 19:12:34 +0900

    apply Perk API swagger #27

--- experiences/serializers/response_serializers.py
+++ experiences/serializers/response_serializers.py
@@ -6,3 +6,8 @@ class PerkResponseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Perk
         fields = "__all__"
+
+
+class PerkListSerializer(serializers.Serializer):
+    total = serializers.IntegerField()
+    records = PerkResponseSerializer(many=True)
