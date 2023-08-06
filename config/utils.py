d9d4e8cc1a7365723b18b2c611df9233bc38462f
hanjuren <juren528@gmail.com>
2023-08-06 18:26:09 +0900

    POST API #29

--- config/utils.py
+++ config/utils.py
@@ -9,7 +9,7 @@ def custom_exception_handler(exc, context):
 
     response = exception_handler(exc, context)
 
-    if response is not None:
+    if response is not None and response.data.get("detail"):
         response.data['message'] = response.data.pop('detail', '')
 
     return response
