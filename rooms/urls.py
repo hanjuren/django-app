e24cf0c7473d490a1c4f3944d672775497c2e94f
hanjuren <juren528@gmail.com>
2023-08-06 16:12:32 +0900

    GET /rooms, /rooms/1 API #29

--- rooms/urls.py
+++ rooms/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path("", views.see_all_rooms),
-    # path("<int:id_>", views.see_one_room),
+    path("", views.Rooms.as_view()),
+    path("/<int:id_>", views.RoomDetail.as_view()),
     # path("<str:name>", views.see_one_order),
     path("/amenities", views.Amenities.as_view()),
     path("/amenities/<int:id_>", views.AmenityDetail.as_view()),
