67be85d6351ce08931a5c8f2c53dd293d9578eaa
hanjuren <juren528@gmail.com>
2023-08-06 16:12:54 +0900

    development, test SQL logging #29

--- config/settings/development.py
+++ config/settings/development.py
@@ -21,3 +21,20 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        }
+    },
+    'loggers': {
+        'django.db.backends': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+    }
+}
