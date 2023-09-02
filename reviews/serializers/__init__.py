c4c1d546ab1f49aff3a2d79eb333f9b4726757ec
hanjuren <juren528@gmail.com>
2023-09-02 15:33:02 +0900

    room.reviews 역참조 #29

---
+++ reviews/serializers/__init__.py
@@ -0,0 +1,1 @@
+from reviews.serializers.response_serializers import ReviewsResponseSerializer
---
+++ reviews/serializers/response_serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from reviews.models import Review
+
+
+class ReviewsResponseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = (
+            "id",
+            "payload",
+            "rating",
+            "user_id",
+            "room_id",
+            "experience_id",
+            "created_at",
+            "updated_at",
+        )
