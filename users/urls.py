84c6816367ae1871dcef9fe39e242c869f4beaf3
hanjuren <juren528@gmail.com>
2023-12-08 16:21:18 +0900

    POST /users #37

--- users/urls.py
+++ users/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("", views.Users.as_view()),
     path("/me", views.Me.as_view()),
     path("/me/avatar", views.Avatar.as_view()),
 ]
--- users/views.py
+++ users/views.py
@@ -1,8 +1,26 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.status import HTTP_201_CREATED
 from rest_framework.permissions import IsAuthenticated
 from drf_yasg.utils import swagger_auto_schema
-from users.serializers import PrivateUserSerializer, UserUpdateSerializer, UserAvatarUpdateSerializer
+from users.serializers import PrivateUserSerializer, UserCreationSerializer, \
+    UserUpdateSerializer, UserAvatarUpdateSerializer
+
+class Users(APIView):
+    swagger_auto_schema(
+        responses={201: PrivateUserSerializer()},
+        tags=["users"]
+    )
+    def post(self, request):
+        serializer = UserCreationSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+
+        return Response(
+            PrivateUserSerializer(user).data,
+            status=HTTP_201_CREATED
+        )
+
 
 class Me(APIView):
     permission_classes = [IsAuthenticated]
@@ -15,6 +33,10 @@ class Me(APIView):
         user = request.user
         return Response(PrivateUserSerializer(user).data)
 
+    @swagger_auto_schema(
+        responses={200: PrivateUserSerializer()},
+        tags=["me"]
+    )
     def put(self, request):
         user = request.user
         serializer = UserUpdateSerializer(user, data=request.data)
@@ -27,6 +49,10 @@ class Me(APIView):
 class Avatar(APIView):
     permission_classes = [IsAuthenticated]
 
+    @swagger_auto_schema(
+        responses={200: PrivateUserSerializer()},
+        tags=["users/avatar"]
+    )
     def put(self, request):
         user = request.user
         serializer = UserAvatarUpdateSerializer(user, data=request.data)
@@ -35,6 +61,9 @@ class Avatar(APIView):
 
         return Response(PrivateUserSerializer(user).data)
 
+    @swagger_auto_schema(
+        tags=["users/avatar"]
+    )
     def delete(self, request):
         user = request.user
         user.delete_avatar()
