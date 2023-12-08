d2d7ffbf20d3e089787196027d6323faf804a7c6
hanjuren <juren528@gmail.com>
2023-12-08 16:21:18 +0900

    POST /users #37

--- users/serializers/validation_serializers.py
+++ users/serializers/validation_serializers.py
@@ -1,5 +1,31 @@
 from rest_framework import serializers
+from rest_framework.validators import UniqueTogetherValidator
 from users.models import User
+
+class UserCreationSerializer(serializers.Serializer):
+    email = serializers.EmailField(max_length=255)
+    password = serializers.CharField()
+    name = serializers.CharField(max_length=10)
+    gender = serializers.ChoiceField(choices=User.GenderChoices)
+    language = serializers.ChoiceField(choices=User.LanguageChoices)
+    currency = serializers.ChoiceField(choices=User.CurrencyChoices)
+    is_host = serializers.BooleanField(default=False)
+
+    def create(self, validated_data):
+        user = User(
+            email=validated_data.get('email'),
+            name=validated_data.get('name'),
+            gender=validated_data.get('gender'),
+            language=validated_data.get('language'),
+            currency=validated_data.get('currency'),
+            is_host=validated_data.get('is_host')
+        )
+        user.set_password(validated_data.get('password'))
+        user.save()
+        return user
+
+    class Meta:
+        validators = [UniqueTogetherValidator(queryset=User.objects.all(), fields=['email'])]
 
 
 class UserUpdateSerializer(serializers.Serializer):
